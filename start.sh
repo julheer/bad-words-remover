a9c06dd19814ef1bb8ba3b28727ac9133fb2498f
Aleksey Demchenkov <admin@julheer.ru>
2021-03-20 20:48:53 +0300

    Added Linux (bash) starter file & remove all cache.

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+do
+	echo "Starting application in 3 seconds..."
+	sleep 3
+		
+	# Change "python" to "python3" if you have multiple versions of Python installed.
+	python src/app.py
+done
